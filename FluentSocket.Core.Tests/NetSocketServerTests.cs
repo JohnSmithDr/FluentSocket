8dfa251ba513c2c43447663cd9bc4846fa2e82e8
johnsmith1st <johnsmith1st@live.com>
2016-10-12 00:28:24 +0800

    rename test class

---
+++ FluentSocket.Core.Tests/NetSocketServerTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using FluentSocket;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class NetSocketServerTests
+    {
+        const int ServerPort = 3000;
+
+        [Fact]
+        public async Task TestListenAsync()
+        {
+            var server = NetSocketFactory.Default.CreateServer();
+            var run = server.ListenAsync(ServerPort);
+            await Task.Delay(50);
+            //Assert.Equal("0.0.0.0", server.LocalEndPoint.Host);
+            //Assert.Equal(ServerPort.ToString(), server.LocalEndPoint.Port);
+            server.Close();
+            server.Dispose();
+        }
+
+        [Fact]
+        public void TestClosed() 
+        {
+            
+        }
+    }
+}
